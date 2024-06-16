a2fe5e053c8753cfecff3f841d3c01e770380037
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-06-15 21:33:38 -0500

    feat: best solution for euler2

--- python/Euler_2.py
+++ python/Euler_2.py
@@ -1,22 +1,10 @@
-# Project Euler Problem #3
-import math
-
-def is_fibonacci(n):
-    # Check if the number is a perfect square
-    root1 = math.isqrt(5*n*n + 4)
-    root2 = math.isqrt(5*n*n - 4)
-    if root1**2 == 5*n*n + 4 or root2**2 == 5*n*n - 4:
-        return True
-    else:
-        return False
-
-def comparation():
-    add = 0
-    for i in range(1,10):
-        if is_fibonacci(i)==True and i%2==0:
-            add += i
-    return add
+def even_fibonacci_sum(limit):
+    a, b = 2, 8
+    total_sum = a
+    while b <= limit:
+        total_sum += b
+        a, b = b, 4 * b + a
+    return total_sum
 
 if __name__ == "__main__":
-    print(comparation())
-
+    print(even_fibonacci_sum(4000000))
